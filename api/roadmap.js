d68e2782f55010a5d280451394cf9484a267b225
Matthew Bain <66839492+rocketstack-matt@users.noreply.github.com>
2024-06-13 12:07:21 +0100

    Update roadmap.js

--- api/roadmap.js
+++ api/roadmap.js
@@ -1,15 +1,19 @@
 const { generateRoadmapSVG, fetchIssues } = require('../roadmap');
 
 module.exports = async (req, res) => {
-    const { owner, repo, colorScheme = 'dark' } = req.query;
+  const { owner, repo, colorScheme = 'dark' } = req.query;
 
-    try {
-        const issues = await fetchIssues(owner, repo);
-        const svgContent = generateRoadmapSVG(issues, colorScheme);
+  if (!owner || !repo) {
+    return res.status(400).send('Missing owner or repo parameter');
+  }
 
-        res.setHeader('Content-Type', 'image/svg+xml');
-        res.send(svgContent);
-    } catch (error) {
-        res.status(500).send('Error fetching GitHub issues');
-    }
+  try {
+    const issues = await fetchIssues(owner, repo);
+    const svgContent = generateRoadmapSVG(issues, colorScheme);
+
+    res.setHeader('Content-Type', 'image/svg+xml');
+    res.send(svgContent);
+  } catch (error) {
+    res.status(500).send('Error fetching GitHub issues');
+  }
 };
